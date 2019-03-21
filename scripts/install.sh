f32edae8e8b1830f348454ed14f16ed2a0b59711
猫科龙 <974985526@qq.com>
2019-03-21 13:18:22 +0800

    修复：文件系统挂载后所有者将变为用户
    之前所有流程执行完之后所有者是 root，是因为 mount 操作会改变目录的所有者。简单起见，在挂载之后将目录的所有者 chown 成当前用户。

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,10 @@
 set -e
 
 # switch working dir and load user settings
-SCRIPT_DIR=$(cd `dirname $0`; pwd)
+SCRIPT_DIR=$(
+  cd $(dirname $0)
+  pwd
+)
 cd $SCRIPT_DIR
 source config.sh
 
@@ -12,11 +15,7 @@ make
 
 # mkdir, unmount and rmmod
 if [[ ! -d $CONFIG_FS_DIR ]]; then
-  USER_NAME=$USER
   sudo mkdir -p $CONFIG_FS_DIR
-  sudo chown $USER_NAME $CONFIG_FS_DIR
-  #TODO: This does'nt seem to work. Fix me.
-  sudo echo chown $USER_NAME $CONFIG_FS_DIR 
 fi
 
 if [[ "$(mountpoint $CONFIG_FS_DIR)" == *"is a"* ]]; then
@@ -31,3 +30,5 @@ fi
 sudo insmod $CONFIG_FS_NAME.ko measure_timing=0
 sleep 1
 sudo mount -t $CONFIG_FS_NAME -o init $CONFIG_PMEM_DEV_DIR $CONFIG_FS_DIR
+USER_NAME=$USER
+sudo chown $USER_NAME $CONFIG_FS_DIR
