2170887f4edba91d01bd9124290b52fff1b5d177
猫科龙 <974985526@qq.com>
2019-03-25 09:49:49 +0800

    调整：调整了 proc 的定位
    proc 文件一次接收含多个 %d 的输入时，将死循环地执行写句柄。这个暂时无法解决的 bug 驱使着将 proc 的定位，从用户/文件系统交互的接口，变更为文件系统调试器。未来，交互接口将由 ioctl 实现，其具备额外的好处：能以文件系统中的文件为参数，使得文件系统更容易获悉用户希望与哪个文件交互。而 proc 将仅仅作调试用，在开发过程中打印一些我所感兴趣的信息。

--- proc.c
+++ proc.c
@@ -9,31 +9,13 @@
 #include <linux/uaccess.h>
 
 #define BUFSIZE 128
-enum opcode { OP_SYNC_CNT, OP_INVALID };
+enum opcode { OP_INVALID };
 static struct proc_dir_entry *ent;
 
 static ssize_t write_handler(struct file *file, const char __user *ubuf,
                              size_t count, loff_t *ppos) {
   char buf[BUFSIZE];
-  int retval = -EINVAL, opcode, pageSN;
-
-  // 获取并检查参数
-  if (*ppos > 0 || count > BUFSIZE || copy_from_user(buf, ubuf, count)) {
-    printk(KERN_ERR
-           "wpmfs-proc: write handler failed to read user buffer."
-           "Kernel buffer size: %d.\n",
-           BUFSIZE);
-    goto out;
-  };
-  if (sscanf(buf, "%d %d", &opcode, &pageSN) != 2 || opcode > OP_INVALID ||
-      opcode < 0) {
-    printk(KERN_ERR "wpmfs-proc: write handler received illegal opcode %d \n",
-           opcode);
-    goto out;
-  }
-
-  // 处理用户的请求
-  printk(KERN_DEBUG "wpmfs-proc: opcode = %d, pageSN = %d.\n", opcode, pageSN);
+  int retval = -EINVAL;
   retval = 0;
 
 out:
@@ -42,7 +24,6 @@ out:
 
 static ssize_t read_handler(struct file *file, char __user *ubuf, size_t count,
                             loff_t *ppos) {
-  printk(KERN_DEBUG "read handler\n");
   return 0;
 }
 
