1e8b6064d7f7d34cbba3acf0d3c0e50b754c2fc4
猫科龙 <974985526@qq.com>
2019-11-29 18:55:15 +0800

    修复无法正确获取 m4m 及 mptable 的问题
    来自以前的迷惑行为

--- wpmfs_wl.h
+++ wpmfs_wl.h
@@ -21,6 +21,10 @@ struct wpmfs_mptable_meta {
   __le64 num_prealloc_pages;
 };
 
+static inline int frag_mptable_slots(void) {
+  return PAGE_SIZE / sizeof(mptable_slot_t);
+}
+
 static inline struct wpmfs_mptable_meta* wpmfs_get_mptable_meta(
     struct super_block* sb) {
   struct pmfs_sb_info* sbi = PMFS_SB(sb);
@@ -31,15 +35,15 @@ static inline m4m_slot_t* wpmfs_get_m4m(struct super_block* sb,
                                         u64* num_slots) {
   struct wpmfs_mptable_meta* mptable = wpmfs_get_mptable_meta(sb);
   if (num_slots)
-    *num_slots = ((le64_to_cpu(mptable->num_prealloc_pages) + (PAGE_SIZE - 1)) &
-                  PAGE_MASK) >>
-                 PAGE_SHIFT;
+    *num_slots = (le64_to_cpu(mptable->num_prealloc_pages) +
+                  (frag_mptable_slots() - 1)) /
+                 frag_mptable_slots();
   return (m4m_slot_t*)(mptable + 1);
 }
 
 static inline m4m_slot_t* wpmfs_get_m4m_slot(struct super_block* sb,
                                              pgoff_t index) {
-  return wpmfs_get_m4m(sb, NULL) + (index >> PAGE_SHIFT);
+  return wpmfs_get_m4m(sb, NULL) + (index / frag_mptable_slots());
 }
 
 static inline mptable_slot_t* wpmfs_get_pgtable_slot(struct super_block* sb,
@@ -47,7 +51,8 @@ static inline mptable_slot_t* wpmfs_get_pgtable_slot(struct super_block* sb,
   u64 blocknr = le64_to_cpu(wpmfs_get_m4m_slot(sb, index)->frag_blocknr);
   u64 blockoff = pmfs_get_block_off(sb, blocknr, PMFS_BLOCK_TYPE_4K);
 
-  return (mptable_slot_t*)pmfs_get_block(sb, blockoff) + (index & ~PAGE_MASK);
+  return (mptable_slot_t*)pmfs_get_block(sb, blockoff) +
+         index % frag_mptable_slots();
 }
 
 /*************************************************
@@ -66,7 +71,7 @@ extern void wpmfs_set_wl_switch(int wlsw);
 extern int wpmfs_init(struct super_block* sb, u64* reserved_memory_size);
 extern void wpmfs_exit(struct super_block* sb);
 
-extern void wpmfs_print_wl_switch(struct super_block *sb);
+extern void wpmfs_print_wl_switch(struct super_block* sb);
 extern bool wpmfs_wl_stranded_enabled(void);
 extern void wpmfs_print_memory_layout(struct super_block* sb,
                                       unsigned long reserved_size);
