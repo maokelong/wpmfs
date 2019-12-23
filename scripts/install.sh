0e540f0ae3fa2b735d03780f5e9fb3f0a5c24f51
猫科龙 <974985526@qq.com>
2019-12-23 20:52:20 +0800

    扩展：现统一在 config.sh 中配置参数

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,11 @@ source config.sh
 
 # make fs
 cd ..
-make CPPFLAGS=-DSTOP_TRACKING
+if [[ $CONFIG_FS_ENABLE_TRACKING -ne 0 ]]; then
+  make
+else
+  make CPPFLAGS=-DSTOP_TRACKING
+fi
 
 # mkdir, unmount and rmmod
 if [[ ! -d $CONFIG_PATH_FS ]]; then
@@ -27,10 +31,23 @@ if [[ -n "$(lsmod | grep $CONFIG_NAME_FS)" ]]; then
 fi
 
 # insmod and mount
-sudo insmod $CONFIG_NAME_FS.ko measure_timing=0
+sudo insmod $CONFIG_NAME_FS.ko measure_timing=$CONFIG_FS_TIMING
 sleep 1
-sudo mount -t $CONFIG_NAME_FS -o init,vmap,dbgmask=$CONFIG_FS_DBGMASK,wlsw=$CONFIG_FS_WL_SWITCH,alloc=$CONFIG_FS_ALLOCATOR \
-  $CONFIG_PATH_PMEM_DEV $CONFIG_PATH_FS
+
+# 准备好文件系统参数，最后一个逗号放上去也无所谓
+args=""
+if [[ $CONFIG_FS_INIT_HARD -ne 0 ]]; then
+  args="init,"
+fi
+if [[ $CONFIG_FS_ENABLE_VMAP -ne 0 ]]; then
+  args=$args"vmap,"
+fi
+args=$args"dbgmask=$CONFIG_FS_DBGMASK,"
+args=$args"alloc=$CONFIG_FS_ALLOCATOR,"
+args=$args"wlsw=$CONFIG_FS_WL_SWITCH,"
+
+sudo mount -t $CONFIG_NAME_FS -o $args $CONFIG_PATH_PMEM_DEV $CONFIG_PATH_FS
+
 USER_NAME=$USER
 sudo chown $USER_NAME $CONFIG_PATH_FS
 sudo chown $USER_NAME $CONFIG_PATH_PROC
